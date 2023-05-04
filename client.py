145f2c9f1a15d603fc943712757baca7fb5cc162
Matthew Gale <matthew.gale@vodafone.com>
2023-05-04 16:22:36 +0100

    robust input validation added

--- client.py
+++ client.py
@@ -12,16 +12,46 @@ def main():
     s.connect((host, port))  # bind the socket to the port and ip address
 
     print("Successfully connected to the server")
-    while running:
+    while running:  # loop for as many times as the user wants
         i += 1
-        first_name = input("Enter the employee's first name: ")
-        surname = input("Enter the employee's surname: ")
-        age = int(input("Enter the employee's age: "))
-        employed = input("Is the employee currently employed, Yes or No? ")
-        if employed == "Yes" or employed == "Y" or employed == "YES" or employed == "yes":
-            employed = True
-        else:
-            employed = False
+
+        valid = False
+        while not valid:  # keep letting the user input first names until they enter a valid one
+            first_name = input("Enter the employee's first name: ")
+            if first_name.isalpha():
+                first_name.capitalize()
+                valid = True
+            else:
+                print("Invalid input! First name should only contain letters")
+        valid = False
+
+        while not valid:
+            surname = input("Enter the employee's surname: ")
+            if surname.isalpha():
+                surname.capitalize()
+                valid = True
+            else:
+                print("Invalid input! Surname should only contain letters")
+
+        while True:
+            try:
+                age = int(input("Enter the employee's age: "))
+            except ValueError:
+                print("Age must be a valid integer")
+            else:  # if the input is a valid integer, break the loop
+                break
+
+        valid = False
+        while not valid:
+            employed = input("Is the employee currently employed, Yes or No? ")
+            if employed in ['Y', 'YES', 'Yes', 'yes']:
+                employed = True
+                valid = True
+            elif employed in ['N', 'NO', 'no', 'No']:
+                employed = False
+                valid = False
+            else:
+                print("Invalid input! The answer can be 'Yes' or 'No'")
 
         employee_dict = {
             'First Name': first_name,
